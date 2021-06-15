69d7fc8e27b62e6b8c486a5e0f39705a8b7cc081
Luciano Santoleri <luchosantoleri@gmail.com>
2021-06-15 19:32:55 -0300

    Creacion interfaz cancelacion y sus implementaciones

---
+++ src/sitioWeb/CancelacionIntermedia.java
@@ -0,0 +1,10 @@
+package sitioWeb;
+
+public class CancelacionIntermedia implements PoliticaDeCancelacion {
+
+	@Override
+	public void cancelar() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/sitioWeb/SinCancelacion.java
@@ -0,0 +1,10 @@
+package sitioWeb;
+
+public class SinCancelacion implements PoliticaDeCancelacion {
+
+	@Override
+	public void cancelar() {
+		// TODO Auto-generated method stub
+		
+	}
+}
